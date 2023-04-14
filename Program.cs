734a0019c8b876df91f6d667fe329ceec445f832
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-04-14 20:45:38 -0300

    exercício pronto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿double devidendo, divisor, quociente;
+
+Console.WriteLine("== Divisão ==");
+
+Console.Write("Digite o devidendo: ");
+devidendo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o divisor: ");
+divisor = Convert.ToDouble(Console.ReadLine());
+ 
+bool invalido = divisor == 0;
+if (invalido)
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("Não é possível fazer a divisão por zero. ");
+    Console.ResetColor();
+}
+else
+{
+quociente = devidendo / divisor;
+Console.WriteLine($" O resultado da divisao entre {devidendo:N1} por {divisor:N1} é: {quociente:N1}.");
+}
